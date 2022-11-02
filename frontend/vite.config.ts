81164c7068b55c2eea539b72a5a2bfd546288495
Matheus Silva <matheus_lsilva@hotmail.com>
2022-11-02 12:14:27 -0300

    fix: corrigindo erro de compilacao do vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react()],
+    build: {
+        target: 'es2015',
+        lib: {
+            entry: 'src/main.tsx',
+            formats: ['umd'],
+            name: 'App'
+        }
+    }
+});
